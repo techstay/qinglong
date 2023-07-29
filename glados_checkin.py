94ee42c677faa356c67d412c2741a6833eb03aaa
techstay <lovery521@gmail.com>
2023-07-29 18:58:12 +0800

    🐛 修复了glados签到失败的问题

--- glados_checkin.py
+++ glados_checkin.py
@@ -9,14 +9,14 @@ from dotenv import load_dotenv
 import myconfig
 import notify
 
-checkin_url = 'https://glados.rocks/api/user/checkin'
-checkin_status_url = 'https://glados.rocks/api/user/status'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50'
-payload = {"token": "glados.network"}
+checkin_url = "https://glados.rocks/api/user/checkin"
+checkin_status_url = "https://glados.rocks/api/user/status"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50"
+payload = {"token": "glados.one"}
 headers = {
-    'origin': 'https://glados.rocks',
-    'user-agent': user_agent,
-    'content-type': 'application/json;charset=UTF-8',
+    "origin": "https://glados.rocks",
+    "user-agent": user_agent,
+    "content-type": "application/json;charset=UTF-8",
 }
 
 
@@ -35,35 +35,35 @@ def checkin(cookie: Dict[str, str]) -> None:
             cookies=cookie,
         ).json()
 
-        email = checkin_status['data']['email']
-        left_days = round(float(checkin_status['data']['leftDays']))
+        email = checkin_status["data"]["email"]
+        left_days = round(float(checkin_status["data"]["leftDays"]))
 
-        if checkin_result['code'] == 0:
+        if checkin_result["code"] == 0:
             notify.send(
-                'glados签到成功',
+                "glados签到成功",
                 f"账号{email}, 剩余时长{left_days}天",
             )
-        elif checkin_result['code'] == 1:
-            message = checkin_result['message']
+        elif checkin_result["code"] == 1:
+            message = checkin_result["message"]
             notify.send(
-                f'glados已签到: {message}',
+                f"glados已签到: {message}",
                 f"账号{email}, 剩余时长{left_days}天",
             )
         else:
             raise Exception(checkin_result)
     except Exception as e:
-        notify.send('glados签到失败，请检查错误提示', e)
+        notify.send("glados签到失败，请检查错误提示", e)
 
 
 def main():
     load_dotenv()
     if len(myconfig.GLADOS_COOKIES) == 0:
-        notify.send('glados未找到配置', '请先在配置文件中设置你的glados cookie')
+        notify.send("glados未找到配置", "请先在配置文件中设置你的glados cookie")
         exit(1)
 
     for cookie in myconfig.GLADOS_COOKIES:
         checkin(cookie)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
